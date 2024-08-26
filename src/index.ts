ea554311b56d795a1efe3db708c781a395c4bed6
Neng Sitiporn <jigneng1@gmail.com>
2024-08-26 18:07:27 +0700

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Welcome to Plan A Day web API")
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 WebAPI is running at ${app.server?.hostname}:${app.server?.port}`
 );
