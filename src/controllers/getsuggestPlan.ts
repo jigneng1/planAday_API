2d2d510b275e36aa827983d1fa0e11aec194f6e6
Neng Sitiporn <jigneng1@gmail.com>
2024-10-28 13:13:32 +0700

    add : suggestionAPI

--- src/controllers/getsuggestPlan.ts
+++ src/controllers/getsuggestPlan.ts
@@ -1,9 +1,11 @@
 import { postgreClient } from "..";
+import planGenerateModel from "../mongoose/planGenerateModel";
 
 const getSuggestPlan = async (userId: string) => {
   try {
     const getpublicPlansFromOtherUsers = await postgreClient.query(
-      "SELECT pp.plan_id FROM users_plans AS up INNER JOIN public_plan AS pp ON up.plan_id = pp.plan_id WHERE user_id != ($1)",[userId]
+      "SELECT pp.plan_id FROM users_plans AS up INNER JOIN public_plan AS pp ON up.plan_id = pp.plan_id WHERE user_id != ($1)",
+      [userId]
     );
     if (getpublicPlansFromOtherUsers.rows.length === 0) {
       return {
@@ -12,9 +14,28 @@ const getSuggestPlan = async (userId: string) => {
       };
     }
 
+    // get plan detail from mongo by plan_id
+    const getPlan = await planGenerateModel.findById(
+      getpublicPlansFromOtherUsers.rows[0].plan_id
+    );
+
+    const uniqueType = [
+      ...new Set(getPlan?.places.map((place) => place.primaryType)),
+    ];
+
+    const formattedResponse = getpublicPlansFromOtherUsers.rows.map((plan) => {
+      return {
+        plan_id: plan.plan_id,
+        place_num: getPlan?.places.length,
+        uniqueType: uniqueType,
+        imageURL: getPlan?.places[0].photosUrl,
+      };
+    });
+
+    // return category and plan
     return {
       success: true,
-      plansList: getpublicPlansFromOtherUsers.rows.map((plan) => plan.plan_id),
+      plansList: formattedResponse,
     };
   } catch (error) {
     return {
